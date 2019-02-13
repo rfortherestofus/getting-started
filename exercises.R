484bc1562c8f57b2ad172374057ac133fb670dcd
dgkeyes <dgkeyes@gmail.com>
2019-02-12 16:21:03 -0800

    Feb 12

---
+++ exercises.R
@@ -0,0 +1,22 @@
+
+# Install Packages --------------------------------------------------------
+
+# Install the tidyverse and skimr packages using the install.packages function
+
+
+# Load Packages -----------------------------------------------------------
+
+# Load the tidyverse and skimr packages using the library function
+
+
+# Import Data -------------------------------------------------------------
+
+# Import the faketucky data into a data frame called faketucky.
+
+skim(faketucky)
+
+# Examine Data ------------------------------------------------------------
+
+
+
+

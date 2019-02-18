83f8e4803b9b9b82e45c83d0a46ab9f7d294bba9
dgkeyes <dgkeyes@gmail.com>
2019-02-18 10:18:38 -0800

    Add codebook

--- R/setup-package.R
+++ R/setup-package.R
@@ -1,12 +1,18 @@
 library(tidyverse)
 library(here)
 
-# This file brings in assets (data and CSS) from the course assets repo (https://github.com/rfortherestofus/course-assets).
+# This file brings in assets (data, codebook, setup.Rmd and CSS) from the course assets repo (https://github.com/rfortherestofus/course-assets).
 
 # Get Data ----------------------------------------------------------------
 
 download.file("https://github.com/rfortherestofus/course-assets/raw/master/data/faketucky-clean-names.csv",
               destfile = here("data", "faketucky.csv"))
+
+
+# Get Codebook ------------------------------------------------------------
+
+download.file("https://raw.githubusercontent.com/rfortherestofus/course-assets/master/data/faketucky-codebook.txt",
+              destfile = here("data", "faketucky-codebook.txt"))
 
 # Get setup.Rmd -----------------------------------------------------------
 
@@ -15,5 +21,5 @@ download.file("https://github.com/rfortherestofus/course-assets/raw/master/misc/
 
 # Get CSS -----------------------------------------------------------------
 
-# download.file("https://raw.githubusercontent.com/rfortherestofus/course-assets/master/style/style.css",
-#               destfile = here("slides", "style.css"))
+download.file("https://raw.githubusercontent.com/rfortherestofus/course-assets/master/style/style.css",
+              destfile = here("slides", "style.css"))

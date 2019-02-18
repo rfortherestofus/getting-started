ad254e846c06532efce42eb1569c6798fd3ed894
dgkeyes <dgkeyes@gmail.com>
2019-02-18 14:30:13 -0800

    Add solutions etc

---
+++ solutions.R
@@ -0,0 +1,33 @@
+
+# Install Packages --------------------------------------------------------
+
+# Install the tidyverse and skimr packages using the install.packages function
+
+
+# Load Packages -----------------------------------------------------------
+
+# Load the tidyverse and skimr packages using the library function
+
+
+# Import Data -------------------------------------------------------------
+
+# Import the faketucky data into a data frame called faketucky.
+
+
+# Examine Data ------------------------------------------------------------
+
+# Enter the name of your data frame (faketucky) and run it as code to see the output.
+
+
+# Use head() to see the first 10 rows of faketucky.
+
+
+# Use tail() to see the first 20 rows of faketucky.
+
+
+# Open faketucky by clicking on it in the environment/history pane or by using the View() function.
+
+
+# Use the skim() function to examine faketucky.
+
+

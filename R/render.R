19ded66cc9c9a7fe2e96bd1628096d1fa3bc74ca
dgkeyes <dgkeyes@gmail.com>
2019-02-27 17:06:08 -0800

    Do it

--- R/render.R
+++ R/render.R
@@ -5,15 +5,15 @@ library(rmarkdown)
 
 # Render presentation -----------------------------------------------------
 
-render(input = here("slides", "slides.Rmd"), 
-       output_dir = here("slides"),
-       output_file = "index.html",
-       clean = T)
+# render(input = here("slides", "slides.Rmd"), 
+#        output_dir = here("slides"),
+#        output_file = "slides.html",
+#        clean = T)
 
 # Convert to PDF ----------------------------------------------------------
 
 https://github.com/yihui/xaringan/wiki/Export-Slides-to-PDF
 
-webshot(here("slides", "index.html"), 
+webshot(here("slides", "slides.html"), 
         here("slides", "slides.pdf"))
 
